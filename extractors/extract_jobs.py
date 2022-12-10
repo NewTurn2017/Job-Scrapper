2398307ea2b4a95ee73146f65e300eb679fe0e91
jaehyunjang <hyuni2020@gmail.com>
2022-12-10 22:07:46 +0900

    [UPDATE] indeed site scripper with selenium lib

---
+++ extractors/extract_jobs.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+def extract_jobs(term):
+    url = f"https://remoteok.com/remote-{term}-jobs"
+    request = requests.get(url, headers={"User-Agent": "Kimchi"})
+    if request.status_code == 200:
+        soup = BeautifulSoup(request.text, "html.parser")
+        table_body = soup.find("table", id="jobsboard")
+        jobs = table_body.find_all("tr", class_="job")
+        results = []
+        for job in jobs:
+            infos = []
+            td = job.find_all("td", class_="company")
+            anchor = td[0].find(["a"])
+            link = anchor["href"]
+            job_url = f"https://remoteok.com{link}"
+            title = anchor.find("h2").string.strip()
+            company = td[0].find("span", class_="companyLink").find("h3").string.strip()
+            locations = td[0].find_all("div", class_="location")
+            for info in locations:
+                infos.append(info.string)
+
+            job_data = {
+                "link": job_url,
+                "title": title,
+                "company": company,
+                "infos": infos,
+            }
+            results.append(job_data)
+        return results
+    else:
+        return "Can't get jobs."
--- main.py
+++ main.py
@@ -1,21 +1,5 @@
-from requests import get
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from extractors.indeed import extract_indeed_jobs
 
-options = Options()
-Options.add_argument("--no-sandbox")
-Options.add_argument("--disable-dev-shm-usage")
+jobs = extract_indeed_jobs("python")
 
-browser = webdriver.Chrome(options=options)
-
-browser = get("https://www.indeed.com/jobs?q=python&limit=50")
-
-
-print(browser.text)
-# if response.status_code != 200:
-#     print("Cant request page")
-# else:
-#     soup = BeautifulSoup(response.text, "html.parser")
-#     job_list = soup.find("ul", class_="jobsearch-ResultsList")
-#     jobs = job_list.find_all("li", recursive=False)
-#     print(len(jobs))
+print(jobs)
